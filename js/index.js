8b8380e2b9ada37bff4d9289bc97abb282d9e461
qaser <huji@mail.ru>
2021-11-21 08:37:45 +0500

    main: пересмотрел позиционирование кнопки закрытия попапа

--- js/index.js
+++ js/index.js
@@ -9,27 +9,24 @@ let userName = document.querySelector('.intro__user-name');
 // род занятий пользователя
 let userOccupation = document.querySelector('.profile__occupation');
 // всплывающее окно
-let popupToggle = document.querySelector('.popup');
+let popup = document.querySelector('.popup');
 // инпут для имени пользователя (доступ по id)
 let inputNameForm = document.querySelector('#user-name');
 // инпут для профессии пользователя (доступ по id)
 let inputOccupationForm = document.querySelector('#user-occupation');
 
 // функция вставки текущих данных пользователя в форму
-function userDataForm() {
+function userDataInForm() {
   inputNameForm.value = userName.textContent;
   inputOccupationForm.value = userOccupation.textContent;
 }
 
 // функция открытия попапа-формы
-function openPopup() {
-  userDataForm();
-  popupToggle.classList.toggle('popup_opened');
-}
-
-// функция открытия попапа-формы
-function closePopup() {
-  popupToggle.classList.toggle('popup_opened');
+function openClosePopup() {
+  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+    userDataInForm();
+  }
 }
 
 // функция отправки данных, введенных в форму
@@ -37,9 +34,9 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   userName.textContent = inputNameForm.value;
   userOccupation.textContent = inputOccupationForm.value;
-  closePopup();
+  openClosePopup();
 }
 
-buttonOpenPopup.addEventListener('click', openPopup);
-buttonClosePopup.addEventListener('click', closePopup);
+buttonOpenPopup.addEventListener('click', openClosePopup);
+buttonClosePopup.addEventListener('click', openClosePopup);
 formElement.addEventListener('submit', formSubmitHandler);
